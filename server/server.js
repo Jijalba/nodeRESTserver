92048f3147fcad01d73f4428d640204e27c8db88
jijalba <joaquinijalba@gmail.com>
2020-04-03 17:35:16 -0300

    gi

---
+++ server/server.js
@@ -0,0 +1,45 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+ 
+//MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use(bodyParser.json());
+
+
+app.get('/usuario', function (req, res) {
+
+res.json('get usuarios');
+
+});
+
+app.put('/usuario/:id', function (req, res) {
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+    
+});
+ 
+app.post('/usuario', function (req, res) {
+
+    let body = req.body;
+
+if (body.nombre === undefined){
+res.status(400).json({
+    ok: false,
+    mensaje: "No se recibió nombre"
+});
+}
+
+    res.json({
+    persona:body    
+    });
+        
+});
+         
+
+
+app.listen(3000, () => console.log("Escuchando el puerto 3000"));
